651a6bebf57c7a98b675528a2bc795b9097efe80
jplin <jplin950310@gmail.com>
2026-03-13 12:26:22 +0800

    第一次提交

---
+++ app/index.js
@@ -0,0 +1,28 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>EXPO作業</Text>
+      <Text style={styles.text}>姓名：林哲平</Text>
+      <Text style={styles.text}>學號：111319023</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  text: {
+    fontSize: 20,
+  },
+});
